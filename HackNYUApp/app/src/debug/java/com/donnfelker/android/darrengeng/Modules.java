f1f9cd54eeeadead2864274d8f80e72a40d56be2
scalls <scalls@rpi.edu>
2016-02-20 14:40:45 -0500

    Update

---
+++ HackNYUApp/app/src/debug/java/com/donnfelker/android/darrengeng/Modules.java
@@ -0,0 +1,15 @@
+package com.hacknyu.brill.darrengeng;
+
+
+final class Modules {
+    static Object[] list() {
+        return new Object[] {
+                new AndroidModule(),
+                new BootstrapModule()
+        };
+    }
+
+    private Modules() {
+        // No instances.
+    }
+}
