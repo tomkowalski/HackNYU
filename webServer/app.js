8940239e10442e1e6e91b527e9390cd9eff02af6
Tom Kowalski <kowalski.tom0@gmail.com>
2016-02-20 14:59:18 -0500

    inital restful api

--- webServer/app.js
+++ webServer/app.js
@@ -6,6 +6,8 @@ var multer  = require('multer')
 var upload = multer({ dest: 'storage/' })
 var mysql = require('mysql');
 var env = require('node-env-file');
+var rest = require("./rest.js");
+var hash = require('password-hash');
 if(process.env.NODE_ENV != "Production") {
 	env('.env');
 }
@@ -17,10 +19,12 @@ connection.connect();
 app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json 
 app.use(bodyParser.json()); 
+var router = express.Router();
+app.use('/api', router);
 app.set('views', './views');
 app.set('view engine', 'jade');
 app.set('port', (process.env.PORT || 5000))
-
+var rest_router = new rest(router,connection,hash);
 
 app.get('/', function(req, res) {
 	connection.query('SELECT * FROM User', function (error, results, fields) {
